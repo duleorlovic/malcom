c4e9706f3375e6def02a635473d8a78428d79a93
Dusan Orlovic <orlovics@eunet.rs>
2014-01-18 00:52:36 +0100

    adding XEON theme from http://shapebootstrap.net/item/xeon-best-onepage-site-template/ because there is no repeat code on every page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
 jQuery(function($) {
 
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 10000,
+			pause: false
+		});
+	});
+
 	//Ajax contact
 	var form = $('.contact-form');
-		form.submit(function () {
-			$this = $(this);
-			$.post($(this).attr('action'), function(data) {
+	form.submit(function () {
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
 			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
 		},'json');
 		return false;
 	});
 
-	//Goto Top
-	$('.gototop').click(function(event) {
-		 event.preventDefault();
-		 $('html, body').animate({
-			 scrollTop: $("body").offset().top
-		 }, 500);
-	});	
-	//End goto top		
+	//smooth scroll
+	$('.navbar-nav > li').click(function(event) {
+		event.preventDefault();
+		var target = $(this).find('>a').prop('hash');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 500);
+	});
 
+	//scrollspy
+	$('[data-spy="scroll"]').each(function () {
+		var $spy = $(this).scrollspy('refresh')
+	})
+
+	//PrettyPhoto
+	$("a.preview").prettyPhoto({
+		social_tools: false
+	});
+
+	//Isotope
+	$(window).load(function(){
+		$portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : 'li',
+			layoutMode : 'fitRows'
+		});
+		$portfolio_selectors = $('.portfolio-filter >li>a');
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
 });
